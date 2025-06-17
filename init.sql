153283310d22d878f96650abebf5dc0250e59849
vineetsaini97 <vineetsainiy93601@gmail.com>
2025-06-17 13:31:26 +0530

    three-tier deploy

---
+++ init.sql
@@ -0,0 +1,3 @@
+USE school;
+CREATE TABLE student (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(40), roll_number INT, class VARCHAR(16));
+CREATE TABLE teacher (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(40), subject VARCHAR(40), class VARCHAR(16));
